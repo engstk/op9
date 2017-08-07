d6701429845ac0a30a48f0e23f9170d0c0f23313
flar2 <asegaert@gmail.com>
2017-08-06 22:26:34 -0400

    msm_performance: don't limit min and max cpu freq
    (cherry picked from commit 68f22b878830838d18e9e38b4e5abf27e06fa1b9)
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -30,6 +30,8 @@
 #include <linux/topology.h>
 #include <linux/scmi_protocol.h>
 
+static int touchboost = 1;
+
 #define POLL_INT 25
 #define NODE_NAME_MAX_CHARS 16
 
@@ -175,6 +177,24 @@ cleanup:
 }
 
 /*******************************sysfs start************************************/
+static int set_touchboost(const char *buf, const struct kernel_param *kp)
+{
+	int val;
+	if (sscanf(buf, "%d\n", &val) != 1)
+		return -EINVAL;
+	touchboost = val;
+	return 0;
+}
+
+static int get_touchboost(char *buf, const struct kernel_param *kp)
+{
+	return snprintf(buf, PAGE_SIZE, "%d", touchboost);
+}
+static const struct kernel_param_ops param_ops_touchboost = {
+	.set = set_touchboost,
+	.get = get_touchboost,
+};
+device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
 	int i, j, ntokens = 0;
@@ -187,6 +207,10 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 #ifdef CONFIG_CONTROL_CENTER
 	bool boost_on_big_hint = false;
 #endif
+	const char *reset = "0:0 1:0 2:0 3:0 4:0 5:0 6:0 7:0";
+
+	if (touchboost == 0)
+		cp = reset;
 
 	if (!ready_for_freq_updates) {
 		ret = freq_qos_request_init();
@@ -205,7 +229,11 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
+	if (touchboost == 0)
+		cp = reset;
+	else
+		cp = buf;
+
 	cpumask_clear(limit_mask_min);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
@@ -299,6 +327,10 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	struct freq_qos_request *req;
 	int ret = 0;
+	const char *reset = "0:4294967295 1:4294967295 2:4294967295 3:4294967295 4:4294967295 5:4294967295 6:4294967295 7:4294967295";
+
+	if (touchboost == 0)
+		cp = reset;
 
 	if (!ready_for_freq_updates) {
 		ret = freq_qos_request_init();
@@ -317,7 +349,11 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
+	if (touchboost == 0)
+		cp = reset;
+	else
+		cp = buf;
+
 	cpumask_clear(limit_mask_max);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
