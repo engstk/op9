29df0bd30c7e5655cd170029f0d618264ed169b4
engstk <eng.stk@sapo.pt>
2022-09-11 20:13:16 +0100

    drivers: vendor/techpack: fix build errors for clang-15
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- techpack/camera/drivers/oplus/cam_sensor_module/cam_tof8801/tof8801_driver.c
+++ techpack/camera/drivers/oplus/cam_sensor_module/cam_tof8801/tof8801_driver.c
@@ -2967,7 +2967,7 @@ static int tof_input_dev_open(struct input_dev *dev)
 	AMS_MUTEX_UNLOCK(&chip->lock);
 	return error;
 }
-int start_poll_thread()
+int start_poll_thread(void)
 {
 	struct tof_sensor_chip *chip = g_tof_sensor_chip;
 	int error = 0;
@@ -3037,7 +3037,7 @@ int tof_start_thread(void *tof_chip)
 
 }
 
-int tof_oem_start()
+int tof_oem_start(void)
 {
 	struct tof_sensor_chip *chip = g_tof_sensor_chip;
 	/*** Use Polled I/O instead of interrupt ***/
@@ -3110,7 +3110,7 @@ int tof_oem_start()
 }
 
 
-int  tof_stop()
+int  tof_stop(void)
 {
 	struct tof_sensor_chip *chip = g_tof_sensor_chip;
 
@@ -3138,7 +3138,7 @@ int  tof_stop()
 	return 0;
 }
 
-int wait_for_tof_ready()
+int wait_for_tof_ready(void)
 {
 
 	struct i2c_client *client = tof_pdata.client;
